bd0b803e752f058530232745fd844d91d15fe178
esft24 <12-11555@usb.ve>
2017-10-31 18:41:09 -0400

    tests de formulario de registro y buen template

--- apps/login/tests.py
+++ apps/login/tests.py
@@ -5,7 +5,6 @@ from django.http import HttpRequest
 from apps.login.views import index
 
 class HomePageTest(TestCase):
-
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 
--- apps/registro/tests.py
+++ apps/registro/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class RegistroFormTest(TestCase):
+    def setUp(self):
+        self.registroBueno = User.objects.create_user(id=1, first_name='Miguel',
+                        last_name='Canedo', username='miganedo', email='mi.canedo10@gmail.com', password='secret123')
+        self.registroBueno.save()
+
+    def test_IdCorrectamenteAlmacenado(self):
+        self.assertEquals(self.registroBueno.id, 1)
+
+class RegistroPageTest(TestCase):
+	def test_uses_registrarUsuarios_template(self):
+		response = self.client.get('/registro')
+
+		self.assertTemplateUsed(response, 'registrarUsuario.html')
+		self.assertTemplateUsed(response, 'layout.html')
