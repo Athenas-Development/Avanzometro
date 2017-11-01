b27cae36a97c468cf0598a42ae4fdd07de9662e5
Carlos Adolfo Perez Feo <capf96@gmail.com>
2017-10-31 22:20:23 -0400

    registro confirmar contrasenas y requireds

---
+++ static/js/inputsRegistro.js
@@ -0,0 +1,12 @@
+var password = document.getElementById("password1"), confirm_password = document.getElementById("password2");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Las contraseñas no coinciden");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
