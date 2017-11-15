131a143663385f815d38f4b09ceb019590f815a4
esft24 <12-11555@usb.ve>
2017-11-15 10:15:15 -0400

    Ahora si

---
+++ apps/dtres/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DtresConfig(AppConfig):
+    name = 'dtres'
---
+++ apps/dtres/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import json
+# Create your views here.
+
+def dtres(request):
+	porcentaje = [5, 15, 20, 15, 30, 14, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	creditos = ['0', '1-16', '17-32', '33-48', '49-64', '65-80', '81-96',
+				'97-112', '113-128', '129-144', '145-160', '161-176', '177-192',
+				'193-208', '209-224', '225-240', '240+']
+	data2 = []
+	for i in range(17):
+		dictdata = {'porcentaje': porcentaje[i],
+					'creditos': creditos[i]}
+
+		print(dictdata)
+		print('hi')
+		data2.append(dictdata)
+
+	#data = [
+    #  { "Word":"Hello", "Awesomeness":2000 },
+    #  { "Word":"World", "Awesomeness":3000 }
+    #]
+	print('hello')
+	print(data2)
+	data2 = json.dumps(data2)
+
+	return render(request, "d3.html", {'data2':data2})
+
